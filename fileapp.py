50ae86de978aeebd98837d5be2f106e1bff9a76a
cdeel <carson.deel@gmail.com>
2024-04-24 21:46:43 -0400

    / root route handled

--- fileapp.py
+++ fileapp.py
@@ -9,3 +9,41 @@ import shutil
 
 # App
 app = Flask(__name__)
+
+
+@app.route('/')
+def root():
+    return render_template_string('''
+        <html>
+          <head>
+            <title>File manager</title>
+          </head>
+          <body>
+            <div align="center">
+              <h1>Local file system</h1>
+              <p><strong>CWD: </strong>{{ current_working_directory }}</p>
+            </div>
+            
+            
+            <ul>
+              <form action="/md">
+                <input type="submit" value="New folder"/>
+                <input name="folder" type="text" value="new_folder"/>
+              </form>
+              <li><a href="/cd?path=..">..</a></li>
+              {% for item in file_list[0: -1] %}
+                {% if '.' not in item%}
+                  <li><strong><a href="/cd?path={{current_working_directory + '/' + item}}">{{item}}</a></strong><a href="/rm?dir={{item}}"> X</a></li>
+                {% elif '.txt' in item or '.py' in item or '.json' in item %}
+                  <li><strong><a href="/view?file={{current_working_directory + '/' + item}}">{{item}}</a></strong></li>
+                {% else %}
+                  <li>{{item}}</li>
+                {% endif%}
+              {% endfor %}
+            </ul>
+          </body>
+        </html>
+    ''', # use 'dir' command on Windows
+    current_working_directory=os.getcwd(),
+         file_list=subprocess.check_output('ls', shell=True).decode('utf-8').split('\n'))
+    
